fb86a0fc63341ca1baedca8822419e32f7da7317
Ben Weber <benweber@student.ethz.ch>
2015-09-14 19:02:45 +0200

    Finished code generation for assignments

--- features/lfocc/features/x86/backend/generators/AssignmentCodeGenerator.java
+++ features/lfocc/features/x86/backend/generators/AssignmentCodeGenerator.java
@@ -3,6 +3,7 @@ package lfocc.features.x86.backend.generators;
 import lfocc.features.assignments.ast.Assignment;
 import lfocc.features.x86.backend.CodeGeneratorInterface;
 import lfocc.features.x86.backend.RegisterManager;
+import lfocc.features.x86.backend.RegisterManager.Register;
 import lfocc.features.x86.backend.CodeGeneratorHelper.ReturnRegister;
 import lfocc.framework.compiler.Backend.BackendFailure;
 
@@ -15,10 +16,14 @@ public class AssignmentCodeGenerator {
 		src += codeGen.getAddress(assign.getTarget());
 		src += codeGen.dispatch(assign.getValue());
 
-		// TODO: implement
+		Register target = ReturnRegister.getRegister(assign.getTarget());
+		Register value = ReturnRegister.getRegister(assign.getValue());
 		
-		regs.free(ReturnRegister.getRegister(assign.getTarget()));
-		regs.free(ReturnRegister.getRegister(assign.getValue()));
+		src += "   movl %" + value + ", (%" + target + ")\n";
+		
+		regs.free(target);
+		regs.free(value);
+
 		return src;
 	}
 }
