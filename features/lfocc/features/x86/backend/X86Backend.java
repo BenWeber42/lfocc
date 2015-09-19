cfc20388c93bd991f7d25f7bbd0d21a474f4cab7
Ben Weber <benweber@student.ethz.ch>
2015-09-19 17:53:47 +0200

    Cleaned up X86Backend

--- features/lfocc/features/x86/backend/X86Backend.java
+++ features/lfocc/features/x86/backend/X86Backend.java
@@ -31,7 +31,6 @@ public class X86Backend implements Backend {
 		CommandOutput compilationOutput = Command.executeWithOutput(
 				new String[] {"gcc", "-m32", "-o", output.getAbsolutePath(), assembler.getAbsolutePath()});
 		
-		// FIXME: activate again to test backend
 		if (!compilationOutput.success()) {
 			throw new BackendFailure("Failed to compile assembly!\n"
 					+ StringUtil.join("\n", compilationOutput.output()));
@@ -40,51 +39,7 @@ public class X86Backend implements Backend {
 	}
 	
 	private String generateAssembler(ASTNode root) throws BackendFailure {
-		// simple hello world program to test correct gcc call
-
-		// TODO: remove
-		/*
-		String src = "";
-        src += "        .section        .rodata\n";
-        src += ".LC0:\n";
-        src += "        .string \"Hello, World!\"\n";
-        src += "        .text\n";
-        src += "        .globl  main\n";
-        src += "        .type   main, @function\n";
-        src += "main:\n";
-        src += ".LFB0:\n";
-        src += "        .cfi_startproc\n";
-        src += "        leal    4(%esp), %ecx\n";
-        src += "        .cfi_def_cfa 1, 0\n";
-        src += "        andl    $-16, %esp\n";
-        src += "        pushl   -4(%ecx)\n";
-        src += "        pushl   %ebp\n";
-        src += "        .cfi_escape 0x10,0x5,0x2,0x75,0\n";
-        src += "        movl    %esp, %ebp\n";
-        src += "        pushl   %ecx\n";
-        src += "        .cfi_escape 0xf,0x3,0x75,0x7c,0x6\n";
-        src += "        subl    $4, %esp\n";
-        src += "        subl    $12, %esp\n";
-        src += "        pushl   $.LC0\n";
-        src += "        call    puts\n";
-        src += "        addl    $16, %esp\n";
-        src += "        movl    $0, %eax\n";
-        src += "        movl    -4(%ebp), %ecx\n";
-        src += "        .cfi_def_cfa 1, 0\n";
-        src += "        leave\n";
-        src += "        .cfi_restore 5\n";
-        src += "        leal    -4(%ecx), %esp\n";
-        src += "        .cfi_def_cfa 4, 4\n";
-        src += "        ret\n";
-        src += "        .cfi_endproc\n";
-        src += ".LFE0:\n";
-        src += "        .size   main, .-main\n";
-        src += "        .ident  \"GCC: (Ubuntu 4.9.2-10ubuntu13) 4.9.2\"\n";
-        src += "        .section        .note.GNU-stack,\"\",@progbits\n";
-        */
-		
 		assert root instanceof GlobalScope;
-
 		return new CodeGenerator().generate((GlobalScope) root);
 	}
 
